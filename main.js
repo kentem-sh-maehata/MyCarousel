b57aad02a29f4905dd3514ca0a1d9222c3cee845
kentem-sh-maehata <sh-maehata@kentem@.co.jp>
2024-05-27 14:09:14 +0900

    ボタンを押して移動

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
     var ul_1 = document.querySelector('ul');
     var slides_1 = ul_1 === null || ul_1 === void 0 ? void 0 : ul_1.children;
     var currentIdx_1 = 0;
+    var dots_1 = [];
     function updateButtons() {
         prev_1 === null || prev_1 === void 0 ? void 0 : prev_1.classList.remove('hidden');
         next_1 === null || next_1 === void 0 ? void 0 : next_1.classList.remove('hidden');
@@ -12,25 +13,50 @@
         if (slides_1 && currentIdx_1 === (slides_1 === null || slides_1 === void 0 ? void 0 : slides_1.length) - 1)
             next_1 === null || next_1 === void 0 ? void 0 : next_1.classList.add('hidden');
     }
+    function moveSlides() {
+        var slideWidth = 0;
+        if (slides_1 !== undefined) {
+            slideWidth = slides_1[0].getBoundingClientRect().width;
+        }
+        if (ul_1 !== null)
+            ul_1.style.transform = "translateX(".concat(-1 * slideWidth * currentIdx_1, "px)");
+        updateButtons();
+        updateDots();
+    }
+    function setupDots() {
+        var _a;
+        if (slides_1) {
+            var _loop_1 = function (i) {
+                var button = document.createElement('button');
+                button.addEventListener('click', function () {
+                    currentIdx_1 = i;
+                    updateDots();
+                    updateButtons();
+                    moveSlides();
+                });
+                dots_1.push(button);
+                (_a = document.querySelector('nav')) === null || _a === void 0 ? void 0 : _a.appendChild(button);
+            };
+            for (var i = 0; i < (slides_1 === null || slides_1 === void 0 ? void 0 : slides_1.length); i++) {
+                _loop_1(i);
+            }
+            dots_1[0].classList.add('current');
+        }
+    }
+    function updateDots() {
+        dots_1.forEach(function (dot) {
+            dot.classList.remove('current');
+        });
+        dots_1[currentIdx_1].classList.add('current');
+    }
     updateButtons();
+    setupDots();
     next_1 === null || next_1 === void 0 ? void 0 : next_1.addEventListener('click', function () {
         currentIdx_1++;
-        var slideWidth = 0;
-        if (slides_1 !== undefined) {
-            slideWidth = slides_1[0].getBoundingClientRect().width;
-        }
-        if (ul_1 !== null)
-            ul_1.style.transform = "translateX(".concat(-1 * slideWidth * currentIdx_1, "px)");
-        updateButtons();
+        moveSlides();
     });
     prev_1 === null || prev_1 === void 0 ? void 0 : prev_1.addEventListener('click', function () {
-        var slideWidth = 0;
         currentIdx_1--;
-        if (slides_1 !== undefined) {
-            slideWidth = slides_1[0].getBoundingClientRect().width;
-        }
-        if (ul_1 !== null)
-            ul_1.style.transform = "translateX(".concat(-1 * slideWidth * currentIdx_1, "px)");
-        updateButtons();
+        moveSlides();
     });
 }
--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
+
 {
   const next = document.getElementById('next')
   const prev = document.getElementById('prev')
   const ul  = document.querySelector('ul')
   const slides = ul?.children
   let currentIdx = 0
+  let dots:HTMLButtonElement[] = []
 
   function updateButtons(){
     prev?.classList.remove('hidden')
@@ -15,24 +17,51 @@
 
   }
 
+  function moveSlides(){
+    let slideWidth:number = 0
+    if(slides !== undefined){
+      slideWidth = slides[0].getBoundingClientRect().width
+    }
+    if(ul !== null)ul.style.transform = `translateX(${-1 * slideWidth * currentIdx}px)`
+    updateButtons()
+    updateDots()
+  }
+
+  function setupDots(){
+    if(slides){
+      
+      for(let i=0; i<slides?.length;i++){
+        const button = document.createElement('button')
+        button.addEventListener('click',()=>{
+          currentIdx = i
+          updateDots()
+          updateButtons()
+          moveSlides()
+        })
+        dots.push(button)
+        document.querySelector('nav')?.appendChild(button)
+      }
+      dots[0].classList.add('current')
+    }
+  }
+
+  function updateDots(){
+    dots.forEach(dot =>{
+      dot.classList.remove('current')
+    })
+    dots[currentIdx].classList.add('current')
+  }
+
   updateButtons()
+  setupDots()
   next?.addEventListener('click',()=>{
     currentIdx ++
-    let slideWidth:number = 0
-    if(slides !== undefined){
-      slideWidth = slides[0].getBoundingClientRect().width
-    }
-    if(ul !== null)ul.style.transform = `translateX(${-1 * slideWidth * currentIdx}px)`
-    updateButtons()
+    moveSlides()
+    
   })
   prev?.addEventListener('click',()=>{
-    let slideWidth:number = 0
     currentIdx --
-    if(slides !== undefined){
-      slideWidth = slides[0].getBoundingClientRect().width
-    }
-    if(ul !== null)ul.style.transform = `translateX(${-1 * slideWidth * currentIdx}px)`
-    updateButtons()
+    moveSlides()
   })
 
 
